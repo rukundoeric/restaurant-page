7f498a5bea5a6ddd59cfeb65b920974c73fab2e9
Rukundo Eric <ericrukundo005@gmail.com>
2021-01-19 23:16:04 +0200

    Add click listener on tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,35 @@
-export const Navigate = (tab) => {
+export const navigateToHome = () => {
   const home = document.getElementById('home');
   const menu = document.getElementById('menu');
   const contact = document.getElementById('contact');
-   
-  console.log(home);
-  // switch(tab) {
-  //   case 'home': {
-  //     if(!home.classList.contains('active')) {
-  //       home.classList.add('active');
-  //       menu.classList.remove('active');
-  //       contact.classList.remove('active');
-  //     }
-  //     break;
-  //   }
-  //   case 'menu': {
-  //     if (!menu.classList.contains('active')) {
-  //       menu.classList.add('active');
-  //       home.classList.remove('active');
-  //       contact.classList.remove('active');
-  //     }
-  //     break;
-  //   }
-  // }  
+
+  if (!home.classList.contains('active')) {
+    home.classList.add('active');
+    menu.classList.remove('active');
+    contact.classList.remove('active');
+  }
+}
+
+export const navigateToMenu = () => {
+  const home = document.getElementById('home');
+  const menu = document.getElementById('menu');
+  const contact = document.getElementById('contact');
+
+  if (!menu.classList.contains('active')) {
+    menu.classList.add('active');
+    home.classList.remove('active');
+    contact.classList.remove('active');
+  }
+}
+
+export const navigateToContact = () => {
+  const home = document.getElementById('home');
+  const menu = document.getElementById('menu');
+  const contact = document.getElementById('contact');
+
+  if (!contact.classList.contains('active')) {
+    contact.classList.add('active');
+    menu.classList.remove('active');
+    home.classList.remove('active');
+  }
 }
--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,5 +1,6 @@
 import element from '../commons/element';
-import { Navigate } from '../../actions/index';
+import { navigateToHome, navigateToMenu, navigateToContact } from '../../actions/index';
+
 const nav = new element().create('nav').class('navbar');
 const container = new element().create('div').class('container d-flex justify-content-between');
 const title = new element().create('div').class('title');
@@ -11,17 +12,17 @@ const btn1 = new element().create('button').appendTo(btns.element());
 btn1.element().setAttribute('data-target', '#home');
 btn1.element().setAttribute('data-toggle', 'tab');
 new element().createText('Home').appendTo(btn1.element());
-btn1.element().addEventListener('click', Navigate);
+btn1.element().addEventListener('click', navigateToHome);
 const btn2 = new element().create('button').appendTo(btns.element());
 btn2.element().setAttribute('data-target', '#menu');
 btn2.element().setAttribute('data-toggle', 'tab');
 new element().createText('Menu').appendTo(btn2.element());
-btn2.element().addEventListener('click', Navigate);
+btn2.element().addEventListener('click', navigateToMenu);
 const btn3 = new element().create('button').appendTo(btns.element());
 btn3.element().setAttribute('data-target', '#contact');
 btn3.element().setAttribute('data-toggle', 'tab');
 new element().createText('Contact Us').appendTo(btn3.element());
-btn3.element().addEventListener('click', Navigate);
+btn3.element().addEventListener('click', navigateToContact);
 btns.appendTo(container.element());
 container.appendTo(nav.element());
 
